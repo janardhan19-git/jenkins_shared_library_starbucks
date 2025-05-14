ea5135c47119d3160652aa89b467d41a7ae003f3
janardhan19-git <janardhanatlassian@gmail.com>
2025-05-14 14:46:23 +0530

    Update runContainer.groovy

--- vars/runContainer.groovy
+++ vars/runContainer.groovy
@@ -1,3 +1,3 @@
 def call() {
- sh "docker run -d - name starbucks -p 3000:3000 janardhanmittapalli/starbucks:latest"
+ sh "docker run -d --name starbucks -p 3000:3000 janardhanmittapalli/starbucks:latest"
 }
